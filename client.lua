4f99cabc49c6a9adbcc0b6ae4043ff0fb4d34445
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-01-09 17:26:37 +0100

    fix des marqueurs

--- client.lua
+++ client.lua
@@ -4,6 +4,7 @@ ESX = exports['es_extended']:getSharedObject()
 local pedModel = "a_m_m_farmer_01"
 local x, y, z = 966.145081, -2128.694580, 31.453491
 local chasseur = false
+local cooldown = 0
 
 Citizen.CreateThread(function()
     RequestModel(GetHashKey(pedModel))
@@ -69,6 +70,7 @@ Citizen.CreateThread(function()
 end)
 
 local animalPositions = {}
+local markedEntities = {}
 
 Citizen.CreateThread(function()
     while true do
@@ -88,9 +90,10 @@ Citizen.CreateThread(function()
             local animalName = animalModels[model]
 
             if animalName then
-                if IsEntityDead(entity) then
+                if IsEntityDead(entity) and not IsEntityMarked(entity) then
                     local animalPosition = GetEntityCoords(entity)
                     table.insert(animalPositions, animalPosition)
+                    table.insert(markedEntities, entity)
                     print("Animal tué à la position : " .. animalPosition.x .. ", " .. animalPosition.y .. ", " .. animalPosition.z)
                 end
             end
@@ -107,7 +110,18 @@ Citizen.CreateThread(function()
     end
 end)
 
-local cooldown = 0
+function IsEntityMarked(entity)
+    for i, markedEntity in ipairs(markedEntities) do
+        if entity == markedEntity then
+            return true
+        end
+    end
+    return false
+end
+
+function RemoveAnimalMarker(index)
+    table.remove(animalPositions, index)
+end
 
 Citizen.CreateThread(function()
     while true do
@@ -118,27 +132,23 @@ Citizen.CreateThread(function()
         for i, animalPosition in ipairs(animalPositions) do
             local distance = #(vector3(animalPosition.x, animalPosition.y, animalPosition.z) - playerCoords)
 
-            if distance <= 2.0 then
-                if IsControlJustPressed(1, 38) and cooldown == 0 then
-                    cooldown = 1000
-                    print("Touche E enfoncée")
-                    RequestAnimDict("amb@medic@standing@kneel@base")
-                    while not HasAnimDictLoaded("amb@medic@standing@kneel@base") do
-                        Citizen.Wait(0)
+            if distance <= 1.0 then
+                if IsControlJustPressed(1, 38) then
+                    local closestMarkerIndex = nil
+                    local closestDistance = 1000.0
+                    for j, markerPosition in ipairs(animalPositions) do
+                        local markerDistance = #(vector3(markerPosition.x, markerPosition.y, markerPosition.z) - playerCoords)
+                        if markerDistance < closestDistance then
+                            closestMarkerIndex = j
+                            closestDistance = markerDistance
+                        end
                     end
-                    TaskPlayAnim(playerPed, "amb@medic@standing@kneel@base", "base", 8.0, -8.0, -1, 0, 0, false, false, false)
-                    TriggerEvent("chat:addMessage", {
-                        color = { 255, 0, 0},
-                        multiline = true,
-                        args = {"Débug", "Animation lancée"}
-                    })
+
+                    if closestMarkerIndex ~= nil then
+                        RemoveAnimalMarker(closestMarkerIndex)
+                    end
                 end
             end
-        end
-
-        if cooldown > 0 then
-            cooldown = cooldown - 1
-            Citizen.Wait(1)
         end
     end
 end)
